39b569dc411acffd48ce59e410f7d40bc3fb4f35
nuralamgarut2311 <nuralamgarut2311@gmail.com>
2025-10-11 23:02:08 +0700

    Restore App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
-// import React from "react";
-
-// export default function App() {
-//   return (
-//     <>
-    
-//     </>
-//   );
-// }
-
 import React from "react";
-import DashboardUser from "./pages/user/DashboardUser";
 
-function App() {
-  return <DashboardUser />;
+export default function App() {
+  return (
+    <>
+    
+    </>
+  );
 }
-
-export default App;
